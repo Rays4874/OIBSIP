7afe6acab10cd25d77b2024ce34c75dafce9c98b
Chayan Das <chayandas952@gmail.com>
2026-06-30 19:53:38 +0530

    Update main.py

--- Random_Password_Generator/main.py
+++ Random_Password_Generator/main.py
@@ -3,7 +3,6 @@ from ui import PasswordGeneratorApp
 
 def main():
     root = tk.Tk()
-    # Apply standard Tkinter styling tweaks if desired
     app = PasswordGeneratorApp(root)
     root.mainloop()
 
